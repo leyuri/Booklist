16db661831453772557a462fdddc8c027a4fea90
leyuri <leyuri97@gmail.com>
2020-04-11 01:32:00 +0900

    Add TopNav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Grid from '@material-ui/core/Grid';
+import TobNav from './components/TopNav';
+import BookList from './components/BookList';
+import BookDetail from './components/BookDetail';
+import Container from '@material-ui/core/Container';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TobNav/>
+      <Container>
+      <Grid container spacing={3}> 
+        <Grid item xs={8}>
+        <BookList/>
+        </Grid>
+        <Grid item xs={4}>
+        <BookDetail/>
+        </Grid>
+      </Grid>
+
+      </Container>
+
     </div>
   );
 }
