0d07787980af9740f20e4053db1f4bdbe30e0f5c
leyuri <leyuri97@gmail.com>
2020-04-12 14:31:34 +0900

    Fix bug

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -38,11 +38,14 @@ function BookDetail({book}) {
         title={book.title}
         subheader={book.subtitle}
       />
-      <CardMedia
-        className={classes.media}
-        image={book.img}
-      />
-
+      {
+        book.img && (
+            <CardMedia
+            className={classes.media}
+            image={book.img}
+          />
+        )
+      }
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
